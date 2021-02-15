1d72fcc0035a0df7899d2ef6e86886fc8106884e
Ayush Gupta <721ayush@gmail.com>
2021-02-15 19:29:51 +0530

    all done

---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+/*
+ *   Copyright (c) 2021
+ *   All rights reserved.
+ */
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '1b29b77f-35dc-4853-8906-003f3ef1749d';
+export const MODAL_CLOSE_SEC = 2.5;
